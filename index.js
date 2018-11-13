91f34763df16e26081822c6487aab8f072251089
milanla <milan.lanlada@gmail.com>
2018-11-13 12:13:58 -0500

    added toggle functions

--- index.js
+++ index.js
@@ -1,9 +1,69 @@
 const newGameBtn = document.getElementById('new-game')
+const colorContainer = document.querySelector('.container')
+const box = document.querySelectorAll('.box')
+let lastBox;
+let welcomePage = document.querySelector('.welcome-page')
+let gameSpace = document.querySelector('.game-space')
+let gameOverPage = document.querySelector('.game-over-page')
+let highScorePage = document.querySelector('.highscore-page')
+const counterElem = document.querySelector('#counter')
 
 
-newGameBtn.addEventListener('click', (e)=>{
-  if (e.target.innerHTML === 'New Game'){
-    e.target.parentElement.remove()
-    debugger
+
+// toggle page functions
+newGameBtn.addEventListener('click', toggleGameSpace)
+const highScoresBtn = document.getElementById('highscore-button')
+highScoresBtn.addEventListener('click', toggleScoreBoard)
+function toggleScoreBoard(e) {
+  welcomePage.style.display = 'none'
+  highScorePage.style.display = 'block'
+}
+function toggleGameSpace(e) {
+  welcomePage.style.display = 'none'
+  gameSpace.style.display = 'block'
+}
+
+
+
+//timer
+const counter = {
+  seconds: 15,
+  start() {
+    this.id = setInterval(() => {
+      this.seconds -= 1
+      if (this.seconds === 0) {
+        this.stop()
+      }
+      counterElem.innerHTML = `${this.seconds}`
+    }, 1000)
+  },
+  stop() {
+    clearInterval(this.id)
+    counterElem.innerHTML = '0'
   }
-})
+}
+// counter.start()
+
+
+
+//function randomly selects time between an amount of time
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+//function selects a box randomly
+function randomBox(box) {
+  const idx = Math.floor(Math.random() * box.length);
+  const boxes = box[idx];
+  return boxes
+}
+
+function glow() {
+  const time = randomTime(90, 1000);
+  const randBox = randomBox(box);
+  let classs = randBox.classList
+  classs.add('light');
+  setTimeout(() => {
+    randBox.classList.remove('light');},
+    time)
+}
